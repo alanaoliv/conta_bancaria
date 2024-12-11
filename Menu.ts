081b3f676d3f917f1a78c822c1fe0f56cccbab70
alanaoliv <alanasanches97@gmail.com>
2024-12-11 10:30:37 -0300

    :sparkles: Classe Model Conta

--- Menu.ts
+++ Menu.ts
@@ -1,59 +1,78 @@
 import readlinesync = require('readline-sync');
 import {colors} from "./src/util/Colors"
+import { Conta } from './src/model/Conta';
 
-let opcao: number;
+export function main() {
 
-do{
+    let opcao: number;
 
-    menu();
+    //Cria novas instâncias (Objetos) da Classe Conta
+    const c1 = new Conta(1, 123, 1, "Jonas", 100000.00);
+    c1.visualizar();
     
-    opcao = readlinesync.questionInt();
+    //Saque
+    console.log(c1.sacar(100.00));
+    c1.visualizar();
 
-    if (opcao === 0){
-        about()
-        process.exit(0)
-    }
+    const c2 = new Conta(2, 123, 2, "Aline", 200000);
+    c2.visualizar();
+    //Depósito
 
-    switch(opcao) {
-        case 1:
-            console.log("\nCriar Conta\n");
+    c2.depositar(100.00);
+    c2.visualizar();
 
-            break;
-        case 2:
-            console.log("\nListar todas as Contas\n");
+    do{
 
-            break;
-        case 3:
-            console.log("\nConsultar Dados da Conta por Número\n");
+        menu();
+    
+        opcao = readlinesync.questionInt();
 
-            break;
-        case 4:
-            console.log("\nAtualizar Dados da Conta\n");
+        if (opcao === 0){
+            about()
+            process.exit(0)
+        }
 
-            break;
-        case 5:
-            console.log("\nApagar Conta\n");
+        switch(opcao) {
+            case 1:
+                console.log("\nCriar Conta\n");
 
-            break;
-        case 6:
-            console.log("\nSaque\n");
+                break;
+            case 2:
+                console.log("\nListar todas as Contas\n");
 
-            break;
-        case 7:
-            console.log("\nDepósito\n");
+                break;
+            case 3:
+                console.log("\nConsultar Dados da Conta por Número\n");
 
-            break;
-        case 8:
-            console.log("\nTransferência entre Contas\n");
+                break;
+            case 4:
+                console.log("\nAtualizar Dados da Conta\n");
 
-            break;
-        default:
-            console.log("\nOpção Inválida!\n");
+                break;
+            case 5:
+                console.log("\nApagar Conta\n");
 
-            break;
-    }
+                break;
+            case 6:
+                console.log("\nSaque\n");
 
-}while(true)
+                break;
+            case 7:
+                console.log("\nDepósito\n");
+
+                break;
+            case 8:
+                console.log("\nTransferência entre Contas\n");
+
+                break;
+            default:
+                console.log("\nOpção Inválida!\n");
+
+                break;
+        }
+
+    }while(true)
+}
 
 function menu(): void{
     console.log(colors.bg.black, colors.fg.red)
@@ -81,3 +100,5 @@ function about(){
     console.log("\nhttps://github.com/alanaoliv/conta_bancaria");
     console.log("\n_________________________________________________________", colors.reset)
 }
+
+main();
